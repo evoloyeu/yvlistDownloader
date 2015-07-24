decfcdbf6e4a21d4632cf186600368f3d8814a4f
rexlei <life545@163.com>
2015-07-24 11:47:28 -0700

    add crawling video page spider

---
+++ sptube/sptube/spiders/videoPage.py
@@ -0,0 +1,53 @@
+import json, pickle
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+
+from sptube.items import SptubeItem
+
+
+class VideoPageSpider(Spider):
+	name = 'videoPage'
+	
+	url_prefix = 'https://www.youtube.com'
+	#['https://www.youtube.com/playlist?list=PLRTwKflrxkQCrP1ZbbKpdP8xoQu2AXT74']
+	start_urls = ["https://www.youtube.com/channel/UCY2hVmHJA25ChhfVcDqsOaw/videos"] 
+
+	# def __init__(self, playlistURL=None, *args, **kwargs):
+	# 	super(VideoPageSpider, self).__init__(*args, **kwargs)
+	# 	self.start_urls = [playlistURL]
+
+	def parse(self, response):
+
+		# print response
+		sel = Selector(response)
+		sites = sel.xpath('//h3[@class="yt-lockup-title"]')
+		items = []
+
+		f=open('playlist.txt', 'w')
+		for site in sites:
+			item = SptubeItem()
+			name = site.xpath('a/text()').extract()[0].strip().encode('utf-8')
+			url = site.xpath('a/@href').extract()[0].encode('utf-8')
+
+			item['name'] = name
+			item['url'] = self.url_prefix+url
+			print item
+			items.append(item)
+			# f.write(name+'\n')
+			f.write(self.url_prefix+url+'\n')
+
+		# print '=========='
+		# print items
+		# print '=========='
+		
+		# f.write(items)
+		# f.write(items)
+		# pickle.dump(items, f)
+		f.close()
+
+		return items
+
+
+
+
+
